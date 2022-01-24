8b115e0621642e5ad1b0b5a8344702a3ad68f5a9
yicongliang <yicongliang@tencent.com>
2022-01-24 15:19:51 +0800

    fedavg

--- test_wandb.py
+++ test_wandb.py
@@ -1,18 +1,4 @@
-# import wandb
-
-if __name__ == '__main__':
-    print('Hello', 'oh', sep=',', end=' ')
-    print('my', 'world', sep='', end='!')
-    print('test')
-    # wandb.init(project="my-test-project", entity="toufunao")
-    #
-    # wandb.config = {
-    #     "learning_rate": 0.001,
-    #     "epochs": 100,
-    #     "batch_size": 128
-    # }
-    #
-    # wandb.log({"loss": 111})
-
-    # Optional
-    # wandb.watch(model)
+t = [1]
+s = [t]
+t.append(s)
+print(s[0][1])
