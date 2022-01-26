6d554b3814a71c91a925cc19bdd265c1ea88fb49
yicongliang <yicongliang@tencent.com>
2022-01-26 16:08:36 +0800

    ip list test

--- experiments/distributed/moleculenet/run_fedavg_distributed_pytorch.sh
+++ experiments/distributed/moleculenet/run_fedavg_distributed_pytorch.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+
+echo 'ip list :'
+ipList="10.244.52.7 10.244.36.13 10.244.37.7 10.244.44.7 10.244.50.7 10.244.53.7 10.244.45.8 10.244.43.7 10.244.48.8 10.244.41.7 10.244.56.7 10.244.55.7"
+for i in $ipList; do
+    echo "$i"
+done
+
+
 CLIENT_NUM=$1
 WORKER_NUM=$2
 SERVER_NUM=$3
